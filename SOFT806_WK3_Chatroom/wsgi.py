81cada112475a4e3913a2bab0baf85a6166748d8
Joey <joas001@ess.ais.ac.nz>
2026-02-18 12:43:48 +1300

    Remove __pycache__ from repository

--- SOFT806_WK3_Chatroom/wsgi.py
+++ SOFT806_WK3_Chatroom/wsgi.py
@@ -14,4 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SOFT806_WK3_Chatroom.settings')
 
 application = get_wsgi_application()
+
 app=application

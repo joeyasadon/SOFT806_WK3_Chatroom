8ccb2bdd89e26f1deb7405a4ae7bdcd8d9a75e1b
Joey <joas001@ess.ais.ac.nz>
2026-02-18 10:53:36 +1300

    Initial commit

---
+++ SOFT806_WK3_Chatroom/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for SOFT806_WK3_Chatroom project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SOFT806_WK3_Chatroom.settings')
+
+application = get_wsgi_application()
